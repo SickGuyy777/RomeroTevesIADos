71671f5898112f69f2ba87fdef2e03ecec8eb61d
SickGuyy777 <57241415+KAWkkkk@users.noreply.github.com>
2023-06-07 19:38:01 -0300

    Enemigo patrulla + LOS
    El enemigo no va al waypoint si no esta en su linea de vision
    
    Para terminar al 100% el patrullaje falta el A*

--- RomeroTeves_ParcialDosIA/Assets/Scripts/EnemyController.cs
+++ RomeroTeves_ParcialDosIA/Assets/Scripts/EnemyController.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     FiniteStateMachine _fsm;
+    Vector3 _velocity;
+
+    [HideInInspector]
+    public int currentPatrolNode;
+
+    [SerializeField] float _maxSpeed;
+    [SerializeField] float _maxForce;
+
+    public Transform[] patrolNodes;
+    public LayerMask wallMask;
+    public float nodeRadius;
+    public Color enemyNextNodeColor;
+
+    public Vector3 VELOCITY { get => _velocity; }
 
     void Start()
     {
         _fsm = new FiniteStateMachine();
-        _fsm.AddState(EnemyStates.Patrol, new PatrolState());
+        _fsm.AddState(EnemyStates.Patrol, new PatrolState(this));
         _fsm.AddState(EnemyStates.Persuit, new PersuitState());
         _fsm.AddState(EnemyStates.LookingFor, new LookingForState());
         _fsm.AddState(EnemyStates.Return, new ReturnState());
@@ -19,6 +33,44 @@ public class EnemyController : MonoBehaviour
     private void Update()
     {
         _fsm.Update();
+
+        //CheckerUpdate();
+    }
+
+    public Vector3 Seek(Vector3 targetPos)
+    {
+        Vector3 desired = (targetPos - transform.position).normalized * _maxSpeed;
+        Vector3 steering = Vector3.ClampMagnitude(desired - _velocity, _maxForce);
+        return steering;
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        _velocity = Vector3.ClampMagnitude(_velocity + force, _maxSpeed);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 dir = patrolNodes[currentPatrolNode].position - transform.position;
+
+        if (Physics.Raycast(transform.position, dir, out RaycastHit hitInfo, dir.magnitude, wallMask))
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, hitInfo.point);
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, patrolNodes[currentPatrolNode].position);
+        }
+
+        Gizmos.color = enemyNextNodeColor;
+
+        foreach (var item in patrolNodes)
+        {
+            Gizmos.DrawWireSphere(patrolNodes[currentPatrolNode].position, nodeRadius);
+        }
+
     }
 }
 
--- RomeroTeves_ParcialDosIA/Assets/Scripts/FSM_Enemies/PatrolState.cs
+++ RomeroTeves_ParcialDosIA/Assets/Scripts/FSM_Enemies/PatrolState.cs
@@ -4,18 +4,50 @@ using UnityEngine;
 
 public class PatrolState : States
 {
+    EnemyController _enemy;
+
+    public PatrolState (EnemyController enemy)
+    {
+        _enemy = enemy;
+    }
+
     public override void OnEnter()
     {
+        Debug.Log("Entro a Patrullar");
     }
 
     public override void Update()
     {
-        Debug.Log("Enemy patrol");
-        //hacer funcion de perseguir al enemigo
+        if (InLOS(_enemy.transform.position, _enemy.patrolNodes[_enemy.currentPatrolNode].position))
+        {
+            WaypointsMove();
+        }
+        else
+        {
+            Debug.Log("No se encontro un Nodo"); //hacer A*
+        }
+    }
+
+    void WaypointsMove()
+    {
+        _enemy.AddForce(_enemy.Seek(_enemy.patrolNodes[_enemy.currentPatrolNode].position));
+        if (Vector3.Distance(_enemy.patrolNodes[_enemy.currentPatrolNode].position, _enemy.transform.position) <= _enemy.nodeRadius)
+            _enemy.currentPatrolNode++;
+
+        if (_enemy.currentPatrolNode >= _enemy.patrolNodes.Length)
+            _enemy.currentPatrolNode = 0;
+
+        _enemy.transform.position += _enemy.VELOCITY * Time.deltaTime;
+        _enemy.transform.forward = _enemy.VELOCITY;
+    }
+
+    bool InLOS(Vector3 start, Vector3 end)
+    {
+        Vector3 dir = end - start;
+        return !Physics.Raycast(start, dir, dir.magnitude, _enemy.wallMask);
     }
 
     public override void OnExit()
     {
-
     }
 }
